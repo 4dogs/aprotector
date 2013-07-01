f723f28579a9ea37b882b420075956ae67d91334
charme000 <909152171@qq.com>
2013-07-01 17:46:20 +0800

    详细注解allocThread函数

--- 4dogs_dalvik/src/vm/Thread.cpp
+++ 4dogs_dalvik/src/vm/Thread.cpp
@@ -801,6 +801,7 @@ bool dvmPrepMainThread()
  * Alloc and initialize a Thread struct.
  *
  * Does not create any objects, just stuff on the system (malloc) heap.
+ * 分配和初始化线程结构体
  */
 static Thread* allocThread(int interpStackSize)
 {
@@ -811,7 +812,7 @@ static Thread* allocThread(int interpStackSize)
     if (thread == NULL)
         return NULL;
 
-    /* Check sizes and alignment */
+    /* Check sizes and alignment 检查大小和对齐*/
     assert((((uintptr_t)&thread->interpBreak.all) & 0x7) == 0);
     assert(sizeof(thread->interpBreak) == sizeof(thread->interpBreak.all));
 
@@ -833,6 +834,8 @@ static Thread* allocThread(int interpStackSize)
      * The stack must be aligned on a 4-byte boundary.
      */
 #ifdef MALLOC_INTERP_STACK
+    /* 分配栈帧空间,必须4字节边界对齐*/
+    /*直接申请空间*/
     stackBottom = (u1*) malloc(interpStackSize);
     if (stackBottom == NULL) {
 #if defined(WITH_SELF_VERIFICATION)
@@ -841,8 +844,47 @@ static Thread* allocThread(int interpStackSize)
         free(thread);
         return NULL;
     }
+    /*填充为0xc5*/
     memset(stackBottom, 0xc5, interpStackSize);     // stop valgrind complaints
 #else
+    /*
+    * 函数：void *mmap(void *start,size_t length,int prot,int flags,int fd,off_t offsize);
+
+	参数start：指向欲映射的内存起始地址，通常设为 NULL，代表让系统自动选定地址，映射成功后返回该地址。
+
+	参数length：代表将文件中多大的部分映射到内存。
+
+	参数prot：映射区域的保护方式。可以为以下几种方式的组合：
+	PROT_EXEC 映射区域可被执行
+	PROT_READ 映射区域可被读取
+	PROT_WRITE 映射区域可被写入
+	PROT_NONE 映射区域不能存取
+
+	参数flags：影响映射区域的各种特性。在调用mmap()时必须要指定MAP_SHARED 或MAP_PRIVATE。
+	MAP_FIXED 如果参数start所指的地址无法成功建立映射时，则放弃映射，不对地址做修正。通常不鼓励用此旗标。
+	MAP_SHARED对映射区域的写入数据会复制回文件内，而且允许其他映射该文件的进程共享。
+	MAP_PRIVATE 对映射区域的写入操作会产生一个映射文件的复制，即私人的“写入时复制”（copy on write）对此区域作的任何修改都不会写回原来的文件内容。
+	MAP_ANONYMOUS建立匿名映射。此时会忽略参数fd，不涉及文件，而且映射区域无法和其他进程共享。
+	MAP_DENYWRITE只允许对映射区域的写入操作，其他对文件直接写入的操作将会被拒绝。
+	MAP_LOCKED 将映射区域锁定住，这表示该区域不会被置换（swap）。
+
+	参数fd：要映射到内存中的文件描述符。如果使用匿名内存映射时，即flags中设置了MAP_ANONYMOUS，fd设为-1。有些系统不支持匿名内存映射，则可以使用fopen打开/dev/zero文件，然后对该文件进行映射，可以同样达到匿名内存映射的效果。
+
+	参数offset：文件映射的偏移量，通常设置为0，代表从文件最前方开始对应，offset必须是分页大小的整数倍。
+
+	返回值：
+
+	若映射成功则返回映射区的内存起始地址，否则返回MAP_FAILED(－1)，错误原因存于errno 中。
+
+	错误代码：
+
+	EBADF 参数fd 不是有效的文件描述词
+	EACCES 存取权限有误。如果是MAP_PRIVATE 情况下文件必须可读，使用MAP_SHARED则要有PROT_WRITE以及该文件要能写入。
+	EINVAL 参数start、length 或offset有一个不合法。
+	EAGAIN 文件被锁住，或是有太多内存被锁住。
+	ENOMEM 内存不足。
+    */
+    /*匿名隐射到该进程的地址空间(这样的做法在写入操作时候有优势)*/
     stackBottom = (u1*) mmap(NULL, interpStackSize, PROT_READ | PROT_WRITE,
         MAP_PRIVATE | MAP_ANON, -1, 0);
     if (stackBottom == MAP_FAILED) {
@@ -855,20 +897,36 @@ static Thread* allocThread(int interpStackSize)
 #endif
 
     assert(((u4)stackBottom & 0x03) == 0); // looks like our malloc ensures this
+    // 填充一些信息
     thread->interpStackSize = interpStackSize;
     thread->interpStackStart = stackBottom + interpStackSize;
     thread->interpStackEnd = stackBottom + STACK_OVERFLOW_RESERVE;
 
 #ifndef DVM_NO_ASM_INTERP
+    // 两种类型的table，具体用哪个由InterpBreak中的breakFlag的值来确定
+    // 这两个首地址在对应的汇编文件里面都能找到
+    // 他们的定义类似如下:
+    //.global dvmAsmInstructionStart
+    //.text
+    //dvmAsmInstructionStart:
+    //.long .L_OP_NOP /* 0x00 */
+    //.long .L_OP_MOVE /* 0x01 */
+    //.long .L_OP_MOVE_FROM16 /* 0x02 */
+    //.long .L_OP_MOVE_16 /* 0x03 */
+    //.long .L_OP_MOVE_WIDE /* 0x04 */
+    //.long .L_OP_MOVE_WIDE_FROM16 /* 0x05 */
+    //.....
+    //....
     thread->mainHandlerTable = dvmAsmInstructionStart;
     thread->altHandlerTable = dvmAsmAltInstructionStart;
-    thread->interpBreak.ctl.curHandlerTable = thread->mainHandlerTable;
+    thread->interpBreak.ctl.curHandlerTable = thread->mainHandlerTable;//默认的表
 #endif
 
     /* give the thread code a chance to set things up */
     dvmInitInterpStack(thread, interpStackSize);
 
     /* One-time setup for interpreter/JIT state */
+    // 初始化解释器或者JIT的状态
     dvmInitInterpreterState(thread);
 
     return thread;
